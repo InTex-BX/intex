30c1db8d2e7844dd9553cf0427a4c758e41649ea
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2015-08-31 17:56:58 +0200

    Remove QObject inheritance for Valve/Heater

--- intex/IntexHardware.h
+++ intex/IntexHardware.h
@@ -2,18 +2,13 @@
 
 #include <memory>
 
-#include <QObject>
-#include <QString>
-
 namespace intex {
 namespace hw {
 namespace config {
 struct gpio;
 }
 
-class Valve : public QObject {
-  Q_OBJECT
-
+class Valve {
   struct Impl;
   std::unique_ptr<Impl> d;
 
@@ -27,9 +22,7 @@ public:
   static Valve &outletValve();
 };
 
-class Heater : public QObject {
-  Q_OBJECT
-
+class Heater {
   class Impl;
   std::unique_ptr<Impl> d;
 
